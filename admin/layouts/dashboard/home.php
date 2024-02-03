f5124453e24372baa8e258f315d5cd28b9a1c767
hendri2212 <cowok_cool320@yahoo.co.id>
2024-02-03 17:12:11 +0800

    dashboard

---
+++ admin/layouts/dashboard/home.php
@@ -0,0 +1,3 @@
+<div class="bg-body-tertiary mt-2 p-3">
+    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Id doloribus eos in, atque nobis quia omnis voluptas sed porro, autem culpa quas molestiae soluta hic dolorum quaerat et aliquid maiores.
+</div>
